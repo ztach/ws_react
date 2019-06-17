5149e54e9fcb9f230637c14580988d3333fa81ec
Staszek <ztach@poczta.onet.pl>
2019-06-17 17:44:54 +0200

    Updates

---
+++ precache-manifest.56b8790ffe3533d1f8ca1133b00b74b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d889d3100cdce9673c0",
+    "url": "/ws_react/static/js/runtime~main.cae03910.js"
+  },
+  {
+    "revision": "c6b17887e40722ac9c23",
+    "url": "/ws_react/static/js/main.a4a9d5bd.chunk.js"
+  },
+  {
+    "revision": "b1d6656e1dcbd84b0fd8",
+    "url": "/ws_react/static/js/2.65f13435.chunk.js"
+  },
+  {
+    "revision": "c6b17887e40722ac9c23",
+    "url": "/ws_react/static/css/main.d9cdf27b.chunk.css"
+  },
+  {
+    "revision": "78fd769aca6cdf25af8d873efd47a01c",
+    "url": "/ws_react/index.html"
+  }
+];
